5f6c35caa3891a21a3c6c7320eaefb5c7b3126b5
IceCandle <icecandle2413@gmail.com>
2025-11-29 19:14:01 +0900

    test: illusion bgColor black

--- Assets/Scripts/IllusionColorSwitch.cs
+++ Assets/Scripts/IllusionColorSwitch.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class IllusionColorSwitch : MonoBehaviour
 {
+    public Camera mainCamera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera.backgroundColor = Color.black;
     }
 
     // Update is called once per frame
