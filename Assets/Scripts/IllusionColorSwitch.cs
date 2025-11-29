fb9523e1b028a9e2ea10df61836624c08c6bd124
IceCandle <icecandle2413@gmail.com>
2025-11-29 19:54:25 +0900

    feat: white floor via IllusionColorSwitcher

--- Assets/Scripts/IllusionColorSwitch.cs
+++ Assets/Scripts/IllusionColorSwitch.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class IllusionColorSwitch : MonoBehaviour
 {
     public Camera mainCamera;
+    public List<MeshRenderer> floorRenderers;
+    public Material blackMaterial;
+    public Material whiteMaterial;
     // Start is called before the first frame update
     void Start()
     {
         mainCamera.backgroundColor = Color.black;
+        foreach (MeshRenderer renderer in floorRenderers)
+        {
+            renderer.material = whiteMaterial;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
